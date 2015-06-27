44fa6fa050e75a10de114411231ce26c32dd436e
bhargavbhegde7 <bhargavbhegde7@gmail.com>
2015-06-28 02:41:34 +0530

    discovered auto run app

--- www/js/app.js
+++ www/js/app.js
@@ -11,6 +11,7 @@ angular.module('starter', [
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    alert("ready!");
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
